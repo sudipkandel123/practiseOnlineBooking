c51cd0d79c79e27ce2573f4f27b9e2f22cd2a8d6
Sudip <sudip@Sudips-MacBook-Pro.local>
2019-03-20 15:30:06 +0530

    Added header component and scss styles

---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+  selector : 'projasa-header',
+  templateUrl : './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent
+{
+
+}
